bf6e6f3d244e2314d96bc2192a9f57aa386f9644
MFarooq22 <104709330+MFarooq22@users.noreply.github.com>
2024-01-01 23:36:55 +0500

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,13 @@ app.use('/public/images', express.static(path.join(__dirname, '../public/images'
 // middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());         // to avoid cors errors
+app.use(cors(
+    {
+    origin: ["we-live-soft.vercel.app"],
+    methods: ["GET", "POST"],
+    credentials: true,
+    }
+));         // to avoid cors errors
 app.use(express.json());
 
 app.use('/api', UserRoutes);
