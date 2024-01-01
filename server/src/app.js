4dee16c2421f40a12b6c084c8d68f19a4a68515b
MFarooq22 <104709330+MFarooq22@users.noreply.github.com>
2024-01-01 23:50:38 +0500

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors(
     {
-    origin: ["welivesoft.vercel.app"],
+    origin: ["https://welivesoft.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
     }
